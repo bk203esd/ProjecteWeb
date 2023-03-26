5f6aafbb766d7bf18d03b7516e9666d84337fdda
Laura Haro Escoi <lauraha.escoi@gmail.com>
2023-03-26 12:24:22 +0200

    Update settings.py
    Prova 1

--- projecteWeb/settings.py
+++ projecteWeb/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+#hola
 # 12Factor
 # import environ
 #
