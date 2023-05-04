379b2cad6a704fc562e4d0f4c250514cf1525523
Guillem Mora <gmb5@alumnes.udl.cat>
2023-05-04 10:08:54 +0200

    created forms

---
+++ stardewApp/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Season, Location, Item, Villager, Schedule
+
+
+class SeasonForm(forms.ModelForm):
+    class Meta:
+        model = Season
+        fields = '__all__'
+
+
+class LocationForm(forms.ModelForm):
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+
+class VillagerForm(forms.ModelForm):
+    class Meta:
+        model = Villager
+        fields = '__all__'
+
+
+class ScheduleForm(forms.ModelForm):
+    class Meta:
+        model = Schedule
+        fields = '__all__'
