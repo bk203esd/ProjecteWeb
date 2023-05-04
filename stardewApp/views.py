0ad352b24946aededa7bca2eb4b7ac6441280608
Guillem Mora <gmb5@alumnes.udl.cat>
2023-05-04 10:33:57 +0200

    created models list

--- stardewApp/views.py
+++ stardewApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import SeasonForm, LocationForm, ItemForm, VillagerForm, ScheduleForm
+from django.views.generic import ListView
+from .forms import *
+from .models import Season, Location, Item, Villager, Schedule
 
 
 # Create your views here.
@@ -16,6 +18,12 @@ def create_season(request):
     return render(request, '../templates/forms/create_season.html', {'form': form})
 
 
+class seasonListView(ListView):
+    model = Season
+    template_name = 'lists/season_list.html'
+    context_object_name = 'seasons'
+
+
 @login_required
 def create_location(request):
     if request.method == 'POST':
@@ -26,6 +34,12 @@ def create_location(request):
     else:
         form = LocationForm()
     return render(request, '../templates/forms/create_location.html', {'form': form})
+
+
+class locationListView(ListView):
+    model = Location
+    template_name = 'lists/location_list.html'
+    context_object_name = 'locations'
 
 
 @login_required
@@ -40,6 +54,12 @@ def create_item(request):
     return render(request, '../templates/forms/create_item.html', {'form': form})
 
 
+class itemListView(ListView):
+    model = Item
+    template_name = 'lists/item_list.html'
+    context_object_name = 'items'
+
+
 @login_required
 def create_villager(request):
     if request.method == 'POST':
@@ -52,6 +72,12 @@ def create_villager(request):
     return render(request, '../templates/forms/create_villager.html', {'form': form})
 
 
+class villagerListView(ListView):
+    model = Villager
+    template_name = 'lists/villager_list.html'
+    context_object_name = 'villagers'
+
+
 @login_required
 def create_schedule(request):
     if request.method == 'POST':
@@ -62,6 +88,12 @@ def create_schedule(request):
     else:
         form = ScheduleForm()
     return render(request, '../templates/forms/create_schedule.html', {'form': form})
+
+
+class scheduleLisTView(ListView):
+    model = Schedule
+    template_name = 'lists/schedule_list.html'
+    context_object_name = 'schedules'
 
 
 def home(request):
