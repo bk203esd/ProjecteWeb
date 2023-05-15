3e06fe52e360cb24ed071b60f84b0c63145f68ef
Guillem Mora(T) <bk203esd@gmail.com>
2023-05-15 21:52:49 +0200

    integrate to django (still not working)

--- stardewApp/webscrape.py
+++ stardewApp/webscrape.py
@@ -1,29 +1,24 @@
 import urllib3
 import bs4
 import re
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projecteWeb.settings')
-
-import django
-django.setup()
-from stardewApp.models import Villager
+from .models import Villager
 
 
-class Villager():
-    def __init__(self, name = "", birthday = "", love = [], like = []) -> None:
-        self.name = name
-        self.birthday = birthday
-        self.love = love
-        self.like = like
-
-    def print(self):
-        print(f'Villager: {self.name}, \n\tBirthdate: {self.birthday}\n\tLoves: {self.love}\n\tLikes: {self.like}')
+# class Villager():
+#     def __init__(self, name = "", birthday = "", love = [], like = []) -> None:
+#         self.name = name
+#         self.birthday = birthday
+#         self.love = love
+#         self.like = like
+#
+#     def print(self):
+#         print(f'Villager: {self.name}, \n\tBirthdate: {self.birthday}\n\tLoves: {self.love}\n\tLikes: {self.like}')
 
 
 class WebScrape(object):
     def __init__(self) -> None:
         self.url = "https://stardewvalleywiki.com"
-    
+
     def get_web(self, sufix):
         httppool = urllib3.PoolManager()
 
@@ -32,9 +27,9 @@ class WebScrape(object):
         self.html = resposta.data.decode("utf-8")
 
     def parse_bs4(self):
-        soup = bs4.BeautifulSoup(self.html, features = "html.parser")
-        #print(soup)
-        div_main = soup.find_all('div', attrs={'class':'gallerytext'})
+        soup = bs4.BeautifulSoup(self.html, features="html.parser")
+        # print(soup)
+        div_main = soup.find_all('div', attrs={'class': 'gallerytext'})
         self.data = []
         for ref in div_main:
             villager = ref.find('a').get('title')
@@ -42,8 +37,8 @@ class WebScrape(object):
 
     def parse_villager_bs4(self):
         regex = re.compile('^(?=.*wikitable)(?=.*roundedborder)(?!.*mw-made-collapsible)(?!.*mw-collapsible).*')
-        soup = bs4.BeautifulSoup(self.html, features = 'html.parser')
-        div_love = soup.find_all('table', attrs={'class':regex})[0]
+        soup = bs4.BeautifulSoup(self.html, features='html.parser')
+        div_love = soup.find_all('table', attrs={'class': regex})[0]
         lines = []
         love = []
 
@@ -51,7 +46,7 @@ class WebScrape(object):
         for line in lines:
             love.extend(line.find_all('a')[1])
 
-        div_like = soup.find_all('table', attrs={'class':regex})[1]
+        div_like = soup.find_all('table', attrs={'class': regex})[1]
         lines = []
         like = []
 
@@ -60,7 +55,7 @@ class WebScrape(object):
         for line in lines:
             like.extend(line.find_all('a')[1])
 
-        div_main = soup.find_all('td', attrs={'id':'infoboxdetail'})[0]
+        div_main = soup.find_all('td', attrs={'id': 'infoboxdetail'})[0]
 
         birthDate = div_main.find('a').get('title')
 
@@ -90,27 +85,29 @@ class WebScrape(object):
         self.parse_villager_html()
         return self.data
 
+
 def fill_dades(villager):
     client = WebScrape()
     dades = client.get_villager_data(villager)
-    Villager.objects.create(
+    dbvillager = Villager.objects.create(
         name=villager,
         birthday=dades['birthday'],
-        love=','.join(dades['love']),
-        like=','.join(dades['like'])
+        love=dades['loved'],
+        like=dades['like']
     )
-    
-if __name__ == "__main__":
-    client = WebScrape()
-    villagers = {}
-    dades= client.get_names_data()
-    print(dades)
-    for index, villager in enumerate(dades):
-        if villager != 'Krobus':
-            villagers[villager] = Villager(name=villager)
-        if index > 32: break
+    return dbvillager
 
-    for villager in villagers:
-        print(villager)
-        fill_dades(villager)
-        villagers[villager].print()
+# if __name__ == "__main__":
+#     client = WebScrape()
+#     villagers = {}
+#     dades= client.get_names_data()
+#     print(dades)
+#     for index, villager in enumerate(dades):
+#         if villager != 'Krobus':
+#             villagers[villager] = Villager(name=villager)
+#         if index > 32: break
+#
+#     for villager in villagers:
+#         print(villager)
+#         fill_dades(villager)
+#         villagers[villager].print()
