8959a3d93e56533465ca48ddd4090a09220bec44
Davit Mas <71844784+Davirre2@users.noreply.github.com>
2023-05-18 21:58:50 +0200

    Afegit test update

--- stardewApp/tests/steps/new_villager_steps.py
+++ stardewApp/tests/steps/new_villager_steps.py
@@ -2,9 +2,11 @@ from behave import given, when, then
 from django.db import connection
 from stardewApp.models import Villager
 
+
 @given("a user that tries to add a villager")
 def step_given_user_adds_villager(context):
     context.browser.visit(context.get_url('villagers/add/'))
+
 
 @when("they select the add villager option with a name, birthday and a list of liked and loved items")
 def step_when_select_add_villager_option(context):
@@ -12,11 +14,12 @@ def step_when_select_add_villager_option(context):
     context.browser.fill("birthday", "Summer 27th")
     context.browser.fill("liked items", "Daffodil")
     context.browser.fill("loved items", "Coconut")
-    context.browser.find_by_xpath('//button[text()="Add Villager"]').first.click()
+    context.browser.find_by_xpath('//button[text()="Submit"]').first.click()
+
 
 @then("the villager is stored in the database")
 def step_then_villager_stored_in_database(context):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT COUNT(*) FROM Villager WHERE name = 'Erik' AND birthday = 'Summer 27th'")
+        cursor.execute("SELECT COUNT(*) FROM Villager WHERE name = 'Jaume' AND birthday = 'Summer 27th'")
         count = cursor.fetchone()[0]
         assert count > 0
--- stardewApp/tests/tests.feature
+++ stardewApp/tests/tests.feature
@@ -28,3 +28,11 @@ Feature: Testing adding a villager
         Given a user that tries to add a villager
         When they select the add villager option with a name, birthday and a list of liked and loved items
         Then the villager is stored in the database
+
+#####MODIFYING AN INSTANCE
+Feature: Testing updating a villager
+    Scenario: A villager is updated correctly
+        Given the user that has added a villager wants to modify it
+        When they modify any of the fields on the villager page
+        Then the information is modified in the database
+
