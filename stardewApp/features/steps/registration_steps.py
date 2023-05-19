4b0d91fba7013d46d2113302e0335bae355040d1
Davit Mas <71844784+Davirre2@users.noreply.github.com>
2023-05-19 16:51:13 +0200

    Update de testos

--- stardewApp/features/steps/registration_steps.py
+++ stardewApp/features/steps/registration_steps.py
@@ -9,9 +9,9 @@ def step_given_visitor_wants_to_register(context):
 
 @when('they introduce a valid user and password')
 def step_when_introduce_valid_user_password(context):
-    context.browser.fill("Username", "CarlesM")
-    context.browser.fill("Password", "contrasenya1")
-    context.browser.fill("Password confirmation", "contrasenya1")
+    context.browser.fill("username", "CarlesM")
+    context.browser.fill("password1", "contrasenya1")
+    context.browser.fill("password2", "contrasenya1")
     context.browser.find_by_xpath('//button[text()="Signup"]').first.click()
 
 @then('their data is stored')
@@ -23,9 +23,9 @@ def step_then_data_is_stored(context):
 
 @when('they introduce an invalid user and password')
 def step_when_introduce_invalid_user_password(context):
-    context.browser.fill("Username", "Roberto")
-    context.browser.fill("Password", "contrasenya1")
-    context.browser.fill("Password confirmation", "contrasenya2")
+    context.browser.fill("username", "Roberto")
+    context.browser.fill("password1", "contrasenya1")
+    context.browser.fill("password2", "contrasenya2")
     context.browser.find_by_xpath('//button[text()="Signup"]').first.click()
 
 @then('their data is not stored')
