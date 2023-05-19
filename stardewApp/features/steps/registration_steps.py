6ac1ec4d8fb0d1abc2dd699551b0ea32273ea4c2
Davit Mas <71844784+Davirre2@users.noreply.github.com>
2023-05-19 11:39:30 +0200

    Arreglar els usuaris per tals que siguin diferents

--- stardewApp/features/steps/registration_steps.py
+++ stardewApp/features/steps/registration_steps.py
@@ -17,13 +17,13 @@ def step_when_introduce_valid_user_password(context):
 @then('their data is stored')
 def step_then_data_is_stored(context):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT COUNT(*) FROM auth_user WHERE username = 'Carlesm' AND password = 'contrasenya1'")
+        cursor.execute("SELECT COUNT(*) FROM auth_user WHERE username = 'CarlesM' AND password = 'contrasenya1'")
         count = cursor.fetchone()[0]
         assert count > 0
 
 @when('they introduce an invalid user and password')
 def step_when_introduce_invalid_user_password(context):
-    context.browser.fill("Username", "CarlesM")
+    context.browser.fill("Username", "Roberto")
     context.browser.fill("Password", "contrasenya1")
     context.browser.fill("Password confirmation", "contrasenya2")
     context.browser.find_by_xpath('//button[text()="Signup"]').first.click()
@@ -31,6 +31,6 @@ def step_when_introduce_invalid_user_password(context):
 @then('their data is not stored')
 def step_then_data_is_not_stored(context):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT COUNT(*) FROM auth_user WHERE username = 'Carlesm' AND password = 'contrasenya1'")
+        cursor.execute("SELECT COUNT(*) FROM auth_user WHERE username = 'Roberto' AND password = 'contrasenya1'")
         count = cursor.fetchone()[0]
         assert count == 0
