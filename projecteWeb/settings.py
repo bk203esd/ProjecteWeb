fec9a1383eafa644cf37ea36cb71832a440ff166
Laura Haro Escoi <lauraha.escoi@gmail.com>
2023-05-19 13:16:45 +0200

    Create item i location done

--- projecteWeb/settings.py
+++ projecteWeb/settings.py
@@ -33,7 +33,6 @@ environ.Env.read_env(os.path.join(BASE_DIR_ENV, 'projecteWeb/.env.local'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
